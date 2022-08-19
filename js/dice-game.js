a5dd8a0539a65a6b2856504abe1f18a4fc20e47f
Jose Quidachay <jpquidachay@gmail.com>
2022-08-19 15:31:32 -0400

    first commit

---
+++ js/dice-game.js
@@ -0,0 +1,23 @@
+function rollDice() {
+    let goldCoins = 0;
+    for (let i = 0; i < 10; i++) {
+        const roll = Math.floor(Math.random() * 6)+ 1;
+        alert('You rolled a ' + roll + '.');
+        if (roll === 1) {
+            alert('Game over, no more rolls!');
+            break;
+        }
+        if (roll === 4 && goldCoins > 0) {
+            goldCoins -= 1;
+            alert('You lost 1 gold coin. You have a total of ' + goldCoins + ' gold coins so far.')
+            continue;
+        }
+        if (roll < 5) {
+            continue;
+        }
+        alert('Congratulations, you win ' + roll + ' gold coins!');
+        goldCoins += roll;
+        alert('You have a total of ' + goldCoins + ' gold coins so far.');
+    }
+    alert('You have won a total of ' + goldCoins + ' gold coins!');
+}
